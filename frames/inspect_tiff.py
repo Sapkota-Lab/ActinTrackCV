e7bcdaf41eb393ce0635476d85ec826311a70fdd
Daniel Kee Tam <danielkee09@gmail.com>
2026-06-24 16:58:12 -0400

    Untrack sample frame data from repository

--- frames/inspect_tiff.py
+++
@@ -1,7 +0,0 @@
-import tifffile, numpy as np
-from PIL import Image
-stack = tifffile.imread("1_WT_218/01.tif")
-for c in [0, 1]:
-    img = stack[7, c]  # middle slice, both channels
-    img_8 = (255 * (img.astype(float) - img.min()) / (np.ptp(img) + 1e-9)).astype(np.uint8)
-    Image.fromarray(img_8).save(f"middle_ch{c}.png")
